e282fb7d57994947a5a82b4ad661356841b9b2d4
Jonathan Schein <jonathan.schein@d3sistemas.com.ar>
2016-06-14 13:46:33 -0300

    Modifico el service provider para que directamente levante la configuración de .env y lo haga todo al momento de registrar MP en el contenedor

--- src/Providers/MercadoPagoServiceProvider.php
+++ src/Providers/MercadoPagoServiceProvider.php
@@ -6,22 +6,14 @@ use LivePixel\MercadoPago\MP;
 class MercadoPagoServiceProvider extends ServiceProvider 
 {
 
-	protected $mp_app_id;
-	protected $mp_app_secret;
-
 	public function boot()
 	{
 		
-		$this->publishes([__DIR__.'/../resources/config/mercadopago.php' => config_path('mercadopago.php')]);
-
-		$this->mp_app_id     = config('mercadopago.app_id');
-		$this->mp_app_secret = config('mercadopago.app_secret');
+		
 	}
 
 	public function register()
 	{
-		$this->app->singleton('MP', function(){
-			return new MP($this->mp_app_id, $this->mp_app_secret);
-		});
+            $this->app->singleton('MP', function(){ return new MP(env('MP_APP_ID'), env('MP_APP_SECRET')); });
 	}
 }
